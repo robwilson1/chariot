3362d78ab34dfbb99629d8702f25f5467ded2842
phobos101 <robwilson101@gmail.com>
2015-12-07 14:09:56 +0000

    Fixing UI.router

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular
-  .module('chariotApp', ['ui-router', 'ngResource'])
+  .module('chariotApp', ['ui.router', 'ngResource'])
   .config(MainConfig);
 
 function MainConfig($stateProvider, $urlRouterProvider) {
